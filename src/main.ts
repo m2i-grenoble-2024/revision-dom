c1326fe4b98c693f1f13337a2431b2fbc74097bd
Jean Demel <demeljean@hotmail.fr>
2024-06-19 11:46:50 +0200

    typescripts basics

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
 import './style.css'
+
+
+let maVariable:string = 'amfdlkqjds';
+
+let monTableau:string[] = ['test','toust','tast'];
+monTableau.push('bidule');
+
+//Trois manière de boucler, je préfère le for..of perso
+for (const item of monTableau) {
+  console.log(item);
+}
+//Mais si j'ai besoin de l'index, je ferais un for classique
+for (let index = 0; index < monTableau.length; index++) {
+  const item = monTableau[index];
+  console.log(item); 
+}
+//ça j'utilise pas trop, mais c'est valide
+monTableau.forEach(item => console.log(item));
+
+/**
+ * On doit typer les paramètres des fonctions et il est possible
+ * de typer le retour de la fonction (si on ne le type pas, ts va
+ * le deviner vis à vis de ce qu'on return ou pas)
+ */
+function addition(a:number,b:number):number {
+  return a+b
+}
+
+
+const maDiv = document.querySelector<HTMLDivElement>('#app');
+maDiv.innerHTML = 'truc';
+
